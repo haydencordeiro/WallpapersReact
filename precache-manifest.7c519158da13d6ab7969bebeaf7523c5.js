7eedf285b530d08b9d3370f5ec336eddc24d0e43
hayden cordeiro <cordeirohayden@gmail.com>
2020-05-17 07:06:52 -0700

    Updates

---
+++ precache-manifest.7c519158da13d6ab7969bebeaf7523c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fc889ed8376f1f82397090e3a454980",
+    "url": "/WallpapersReact/index.html"
+  },
+  {
+    "revision": "47093fe9a53223c5b2ce",
+    "url": "/WallpapersReact/static/css/main.567e379c.chunk.css"
+  },
+  {
+    "revision": "f7a06be1fe6f886a4e39",
+    "url": "/WallpapersReact/static/js/2.a22c3296.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/WallpapersReact/static/js/2.a22c3296.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47093fe9a53223c5b2ce",
+    "url": "/WallpapersReact/static/js/main.f8d4d55f.chunk.js"
+  },
+  {
+    "revision": "429ccc2ca446ca787dbb",
+    "url": "/WallpapersReact/static/js/runtime-main.d891498a.js"
+  }
+]);
